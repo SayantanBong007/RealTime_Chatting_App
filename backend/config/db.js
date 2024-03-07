3277d5352ff86f2943c37c68be37d86f6cc2d845
Sayantan Gain <sayantangain63@gmail.com>
2024-03-07 22:38:13 +0530

    Change the database for hosting

--- backend/config/db.js
+++ backend/config/db.js
@@ -4,10 +4,7 @@ const mongoose = require("mongoose");
 const connectDB = async () => {
   try {
     // Attempt to establish a connection to the MongoDB database using the provided URI
-    const conn = await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true, // Use the new URL parser
-      useUnifiedTopology: true, // Use the new server discovery and monitoring engine
-    });
+    const conn = await mongoose.connect(process.env.MONGO_URI);
 
     // If the connection is successful, log a success message
     console.log(`MongoDB connected: ${conn.connection.host}`.cyan.underline);
